f36db7671119ac600e6308b07bf1b7f8592842a0
3Dlyra <3dlyra.com@gmail.com>
2023-02-12 18:12:54 +0500

    Implement `cubic` (Cubic Bézier Curve) method

--- curve3.d.ts
+++ curve3.d.ts
@@ -1,1 +1,2 @@
 export function quadratic( a: Float32Array, b: Float32Array, c: Float32Array, div: number ): Float32Array;
+export function cubic( a: Float32Array, b: Float32Array, c: Float32Array, d: Float32Array, div: number ): Float32Array;
--- curve3.js
+++ curve3.js
@@ -11,3 +11,17 @@ export function quadratic( a, b, c, div ) {
 
 	return out
 }
+
+export function cubic( a, b, c, d, div ) {
+
+	const out = new Float32Array( ( div + 1 ) * 3 )
+
+	for ( let i = 0; i <= div; i++ ) {
+
+		out[ i * 3 ] = ( ( 1 - i / div ) * ( 1 - i / div ) * ( 1 - i / div ) * a[ 0 ] ) + ( 3 * ( 1 - i / div ) * ( 1 - i / div ) * i / div * b[ 0 ] ) + ( 3 * ( 1 - i / div ) * i / div * i / div * c[ 0 ] ) + ( i / div * i / div * i / div * d[ 0 ] )
+		out[ i * 3 + 1 ] = ( ( 1 - i / div ) * ( 1 - i / div ) * ( 1 - i / div ) * a[ 1 ] ) + ( 3 * ( 1 - i / div ) * ( 1 - i / div ) * i / div * b[ 1 ] ) + ( 3 * ( 1 - i / div ) * i / div * i / div * c[ 1 ] ) + ( i / div * i / div * i / div * d[ 1 ] )
+		out[ i * 3 + 2 ] = ( ( 1 - i / div ) * ( 1 - i / div ) * ( 1 - i / div ) * a[ 2 ] ) + ( 3 * ( 1 - i / div ) * ( 1 - i / div ) * i / div * b[ 2 ] ) + ( 3 * ( 1 - i / div ) * i / div * i / div * c[ 2 ] ) + ( i / div * i / div * i / div * d[ 2 ] )
+	}
+
+	return out
+}
